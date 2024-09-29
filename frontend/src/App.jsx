743f6fdc247e3247fa1e0c315b1d510ca6e70eed
LeoWilliam001 <gleowilliam@gmail.com>
2024-09-29 10:33:16 +0530

    second

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1 className="flex justify-center items-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
